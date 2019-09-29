0f18f3df6dd8012d23480cada45ca30a359296e2
Alexei Dias <alexeidias@gmail.com>
2019-09-28 19:07:13 -0700

    spotify working, movies working, tell me a joke is working but not calling menu, bands is broken

---
+++ keys.js
@@ -0,0 +1,13 @@
+var colors = require('colors');
+ var lines = process.stdout.getWindowSize()[1];
+        for(var i = 0; i < lines; i++) {
+        console.log('\r\n');
+        
+        }
+console.log(colors.blue('\nHello, welcome to\n'));
+console.log(colors.yellow.bold("Liri!\n"));
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+  
